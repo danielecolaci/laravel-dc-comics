6a0b96fd38c09df0fb31a4ffcdba74f2289c6b4b
danielecolaci <danielecolaci@outlook.com>
2024-05-17 12:26:05 +0200

    Create migrations, model and seeder, link db

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comics;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics');
+
+        foreach($comics as $comic) {
+            $newComic = new Comics();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
+    }
+}
