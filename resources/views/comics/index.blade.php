964649614ddc00ee910f2c145ce4486fcc618a28
danielecolaci <danielecolaci@outlook.com>
2024-05-19 15:20:02 +0200

    Add Modal on delete action

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -19,12 +19,44 @@
                             <div class="d-flex justify-content-between">
                                 <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary px-3">View</a>
                                 <a href="{{ route('comics.edit', $comic) }}" class="btn btn-warning px-3">Edit</a>
-                                <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="d-inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger px-3"
-                                        onclick="return confirm('Are you sure you want to delete this comic?')">Delete</button>
-                                </form>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $comic->id }}">
+                                    Delete</i>
+                                </button>
+
+                                <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId-{{ $comic->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId-{{ $comic->id }}">
+                                                    You are deleting: {{ $comic->title }}
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Attention! You are about to delete this record. The operation is
+                                                irreversible!
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                    Close
+                                                </button>
+                                                <form action="{{ route('comics.destroy', $comic) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">
+                                                        Confirm
+                                                    </button>
+
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -23,12 +23,44 @@
                         <li class="list-group-item d-flex justify-content-evenly"><a
                                 href="{{ route('comics.edit', $comic) }}" class="btn btn-warning m-4 px-5">Edit Comic</a>
 
-                            <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger m-4 px-5"
-                                    onclick="return confirm('Are you sure you want to delete this comic?')">Delete</button>
-                            </form>
+                            <button type="button" class="btn btn-danger m-4 px-5" data-bs-toggle="modal"
+                                data-bs-target="#modalId-{{ $comic->id }}">
+                                Delete</i>
+                            </button>
+
+                            <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
+                                data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                aria-labelledby="modalTitleId-{{ $comic->id }}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                    role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="modalTitleId-{{ $comic->id }}">
+                                                You are deleting: {{ $comic->title }}
+                                            </h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Attention! You are about to delete this record. The operation is
+                                            irreversible!
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                Close
+                                            </button>
+                                            <form action="{{ route('comics.destroy', $comic) }}" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">
+                                                    Confirm
+                                                </button>
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
